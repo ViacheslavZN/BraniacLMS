dd32d79e65c5bf5144e5d4c8f4f650d14c24f5c2
ViacheslavZN <zyrinviacheslav@gmail.com>
2022-12-18 23:30:49 +0300

    migration created

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -1,1 +1,8 @@
-# Register your models here.
+from django.contrib import admin
+
+from .models import News, Courses, CourseTeachers, Lesson
+
+admin.site.register(News)
+admin.site.register(Courses)
+admin.site.register(CourseTeachers)
+admin.site.register(Lesson)
