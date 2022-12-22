b2a725d1165c35853bd375c16219149656a98f9e
ViacheslavZN <zyrinviacheslav@gmail.com>
2022-12-22 21:59:19 +0300

    NEw html

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -6,10 +6,18 @@ from mainapp.apps import MainappConfig
 app_name = MainappConfig.name
 
 urlpatterns = [
-    path("", views.MainPageView.as_view()),
-    path("news.html/", views.NewsPageView.as_view()),
-    path("courses_list.html/", views.CoursePageView.as_view()),
-    path("contacts.html/", views.ContactPageView.as_view()),
-    path("doc_site.html/", views.DocSitePageView.as_view()),
-    path("login.html/", views.LoginPageView.as_view()),
+    path("", views.MainPageView.as_view(), name="main_page"),
+    path("news/", views.NewsPageView.as_view(), name="news"),
+    path(
+        "news/<int:pk>/", views.NewsPageDetailView.as_view(), name="news_detail"
+    ),
+    path("courses/", views.CoursesListView.as_view(), name="courses"),
+    path(
+        "courses/<int:pk>/",
+        views.CoursesDetailView.as_view(),
+        name="courses_detail",
+    ),
+    path("contacts/", views.ContactsPageView.as_view(), name="contacts"),
+    path("doc_site/", views.DocSitePageView.as_view(), name="doc_site"),
+    path("login/", views.LoginPageView.as_view(), name="login"),
 ]
